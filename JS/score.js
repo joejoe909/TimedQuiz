bf6a2b9fc3048642500c56376b84ee9b26355e8e
Joseph Farrish <joseph_a_f@hotmail.com>
2020-08-11 20:54:59 -0700

    okay not my best work but done...

--- JS/score.js
+++ JS/score.js
@@ -1,42 +1,26 @@
 var score = localStorage.getItem("usrScore");
-var disScore = document.getElementById("scoreID");
+var disScore = document.getElementById("scoreID");       //this displays the screen when the test is complete.
 disScore.textContent = ("Your test is complete, your score is: " + parseInt(score));
 var beginTest = document.querySelector("#beginTest");        //restarts test
 var nameInputBtn = document.querySelector("#EnterName");    //button to submit name.
 var nameString = document.querySelector("#nameID");         //form input text
 var userList = document.getElementById("userList");         //boot strap alert div
 var scrBrd = document.getElementById("scoreboard");
-var aString;
-let st_ScA= [], rt_ScA = [];
-let ScA;
 
 let usrScoreSet = {
     name: '',
     score: '',
 };
+let tScoreSet = Object.create(usrScoreSet);
+let scoreArray = []; //this stores an array of user score sets     
 
-let scrArray = {
-    aScoreSet: [],
-    len:0,
-    addSet: function(aSet){
-        this.aScoreSet.push(aSet);
-        this.len++;
-    }
-};
-
-let score0 = Object.create(usrScoreSet);  //this is a user score set name and score values
-let scoreArray = Object.create(scrArray); //this stores a user score set                                   
-loadScoreboard();
-addToNameBoard();
+init();
+//RenderScoreBoard();
 
 EnterName.addEventListener("click", function (event) { //Enter your name button.
     event.preventDefault();
-    aScore = Object.create(usrScoreSet);
-    aScore.name = nameString.value;
-    aScore.score = parseInt(score);
-    addToScoreArray(aScore);
-    scrBrd.innerHTML ="";
-    addToNameBoard();
+    nameInputBtn.style.display = "none";
+    processName();
 });
 
 beginTest.addEventListener("click", function (event) {
@@ -45,30 +29,11 @@ beginTest.addEventListener("click", function (event) {
     window.location.href = "./test.html";
 });
 
-function addToScoreArray(AuserScoreSet)
-{
-     scoreArray.addSet(AuserScoreSet);
-     for(var i = 0; i < scoreArray.len; i++)
-     {
-         st_ScA[i] = JSON.stringify(scoreArray.aScoreSet[i]);
-         
-     }
-    
-    localStorage.setItem("ScoreBoard", st_ScA);
-}
-
-function loadScoreboard(){
-    
-    ScA = JSON.parse(localStorage.getItem("Scoreboard"));
-
-}
-
-function addToNameBoard(){
+function RenderScoreBoard(){ //
   
-    for(let i =0; i < scoreArray.len; i++)
+    for(let i =0; i < scoreArray.length; i++)
     {   
-        
-        let newSet = scoreArray.aScoreSet[i];
+        let newSet = scoreArray[i];
         let aNewDiv = document.createElement('div');
         let aNewP = document.createElement('p');
         aNewDiv.setAttribute('id', "usrEntry"  + i);
@@ -76,54 +41,43 @@ function addToNameBoard(){
         aNewP.setAttribute('id', 'p'+i);
         aNewP.textContent = "User Name: " + newSet.name + " Score: " + newSet.score;
         aNewDiv.appendChild(aNewP);
-        scrBrd.appendChild(aNewDiv); //append to board.
+        scrBrd.appendChild(aNewDiv); //append to board.    
+        storeArray();
     }
 }
 
-// function gengrid() {
-//     var i = 0;
-//     var num_stud = 8;
-//     var newdiv;
-//     var divIdName;
-//     for (i = 1; i <= num_stud; i++) {
-//         newdiv = document.createElement('div');
-//         divIdName = '50' + i;
-//         newdiv.setAttribute('id', divIdName);
-//         newdiv.innerHTML = '<div id="box' + i + '">Testing 123</div>';
-//         document.body.appendChild(newdiv);
-//     }
-// }
-// window.onload = function () {
-//     gengrid();
-// }
+function processName(){
+    aScoreSet = Object.create(usrScoreSet);
+    aScoreSet.name = nameString.value; //this stores the name.
+    aScoreSet.score = score;  //this stores the score.
+    scoreArray.push(aScoreSet);  
+    scrBrd.innerHTML = "";
+    RenderScoreBoard();
+}
+
+function init(){
+    //get from stored location.
+    var storedNamesScores = JSON.parse(localStorage.getItem("ScoreBoard"));
+    if(storedNamesScores !== null)
+    {
+        scoreArray = [];
+        scoreArray = storedNamesScores;
+        RenderScoreBoard();
+    }    
+}
+
+function addToScoreArray(AuserScoreSet) {
+    scoreArray.push(AuserScoreSet);
+}
 
 
-// function addToNameBoard(aString, score){
-//     scoreBoard = Object.create(aString, score);
-//     scoreBoardArray.push(scoreBoard);
-// }
+function storeArray(){
+     let st_ScA = [];
+    for (var i = 0; i < scoreArray.length; i++) { 
+        localStorage.setItem("ScoreBoard", JSON.stringify(scoreArray));
+    }
+}
 
-
-
-// let userScoreSet = {
-//     name: '',
-//     score: '',
-//     divID: 0,
-
-//     createDiv() {
-//         aNewDiv = document.createElement('div');
-//         aNewDiv.clasName = 'alert alert-danger';
-//         aNewDiv.textContent = this.name, this.score;
-//         aNewDiv.setAttributes("id", parseInt(this.divID));
-//         return aNewDiv;
-//     }
-// };
-
-
-// scoreBoard = Object.create(userScoreSet);
-// scoreBoard.name = "Joseph";
-// scoreBoard.score = "80";
-
-// scoreBoardArray = [scoreBoard];
-
-
+function clearScoreBoard(){
+    scrBrd.innerHTML = "";
+}
--- backupJS/testHolder.js
+++
@@ -1,9 +0,0 @@
-var testHandler = document('qnaCard.js');
-testHandler.src = './qndaCard.js';
-
-var test = {
-    q0: qnaCard,
-    q1: qnaCard,
-    q2: qnaCard,
-    q3: qnaCard,
-}
